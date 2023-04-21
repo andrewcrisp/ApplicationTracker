ab082f05d48f62fb7ea6f7ae49d92500ee504ee3
Andrew Crisp <andrewcrisp556@hotmail.com>
2023-04-21 15:20:41 -0600

    Initial commit

---
+++ ApplicationTracker/Application.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationTracker
+{
+    class Application
+    {
+        public Application(string company,
+            string position,
+            string announcementID,
+            string link,
+            DateTime dateApplied,
+            string currentStatus,
+            string[] notes)
+        {
+            Company = company;
+            Position = position;
+            AnnouncementID = announcementID;
+            Link = link;
+            DateApplied = dateApplied;
+            CurrentStatus = currentStatus;
+            Notes = notes;
+        }
+
+        public string Company { get; set; }
+        public string Position { get; set; }
+        public string AnnouncementID { get; set; }
+        public string Link { get; set; }
+        public DateTime DateApplied { get; set; }
+        public string CurrentStatus { get; set; }
+        public string[] Notes { get; set; }
+
+    }
+}
